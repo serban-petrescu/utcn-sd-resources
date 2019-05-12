ac4d30bfd719dcc801ee2c838241ebed246db149
Serban Petrescu <serban.petrescu@outlook.com>
2019-05-12 17:29:27 +0300

    Adds allowed methods to CORS config.

--- examples/assignment-3/backend/src/main/java/ro/utcn/sd/a3/config/WebConfiguration.java
+++ examples/assignment-3/backend/src/main/java/ro/utcn/sd/a3/config/WebConfiguration.java
@@ -10,6 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
